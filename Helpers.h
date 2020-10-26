80e90ed0b21cc89ef28e21919885c539937b0948
rulflyfly <rulflyfly@gmail.com>
2020-10-26 17:02:57 +0300

    stack implementation

--- Helpers.h
+++ Helpers.h
@@ -1,4 +1,18 @@
-int sqrSum(int num1, int num2)
+struct Node 
 {
-    return (num1 + num2) * (num1 + num2);
-}
+    std::string data;
+    Node *next;
+};
+
+class Stack 
+{
+    public:
+        Stack(); // constructor
+        ~Stack(); // destructor
+        void push(std::string d); // pushes a node on top of stack
+        std::string pop(); // will pop the top node of the stack
+        std::string toString(); // display stack's contents as a string top to bottom
+        bool isEmpty();
+    private:
+        Node *top;
+};
